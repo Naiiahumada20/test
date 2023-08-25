9be2da417074d9341a7e7790ae9eab2ef8cc7334
Naiiahumada20 <nailethbolivar3@gmail.com>
2023-08-25 18:02:25 -0500

    cambios

---
+++ Matematicas/mate.js
@@ -0,0 +1,58 @@
+// creando una matriz y pasando el número, preguntas, opciones y respuestas
+let questions = [
+    {
+    numb: 1,
+    question: "z,electronegatividad,caracter metalico; son ejemplos de:?",
+    answer: "Propiedades fisicas especificas",
+    options: [
+      "Propiedades fisicas especificas",
+      "propiedades fundamentales de la materia",
+      "propiedades fisicas generales",
+      "propiedades especificas quimicas"
+    ]
+  },
+    {
+    numb: 2,
+    question: "¿atomos de un mismo elemento que tienen diferentes masas atomicas?",
+    answer: "isótopos",
+    options: [
+      "gases",
+      "isómeros",
+      "halógeno",
+      "isótopos"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Todas las sustancias que nos rodean estan formadas por:",
+    answer: "Atomos y moleculas",
+    options: [
+      "Atomos",
+      "Moleculas ",
+      "Atomos y moleculas",
+      "Ninguna de las anteriores"
+    ]
+  },
+    {
+    numb: 4,
+    question: "El elemento más abundante en el universo es:",
+    answer: "Hidrogeno",
+    options: [
+      "cloro",
+      "Hidrogeno",
+      "Helio",
+      "Carbono"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Al mezclar dos gases se obtiene una disolución:",
+    answer: "Siempre",
+    options: [
+      "Aveces",
+      "Nunca",
+      "Depente el lugar",
+      "Siempre"
+    ]
+  },
+];
---
+++ español/español.js
@@ -0,0 +1,58 @@
+// creando una matriz y pasando el número, preguntas, opciones y respuestas
+let questions = [
+    {
+    numb: 1,
+    question: "¿Cual es la capital de España?",
+    answer: "Madrid",
+    options: [
+      "Barcelona",
+      "Sevilla",
+      "Madrid",
+      "Valencia"
+    ]
+  },
+    {
+    numb: 2,
+    question: "¿Que significa la palabra amistad",
+    answer: "Relacion de afecto y compañia entre personas",
+    options: [
+      "Tristeza",
+      "Enemistad",
+      "Alegria",
+      "Relacion de afecto y compañia entre personas"
+    ]
+  },
+    {
+    numb: 3,
+    question: "¿Cual de los siguientes planetas es el mas grande del sistema solar?",
+    answer: "Jupiter",
+    options: [
+      "Marte",
+      "Venus ",
+      "Jupiter",
+      "Mercurio"
+    ]
+  },
+    {
+    numb: 4,
+    question: "¿Cual es el rio mas grande del mundo?",
+    answer: "Amazonas",
+    options: [
+      "Nilo",
+      "Amazonas",
+      "Misisipi",
+      "Yangtsé"
+    ]
+  },
+    {
+    numb: 5,
+    question: "¿Quien escribio Don Quijote de la mancha?",
+    answer: "Miguel de Cervantes Saavedra",
+    options: [
+      "Gabriel Garcia Marquez",
+      "Federico Garcia Lorca",
+      "Miguel de Cervantes Saavedra",
+      "Pablo Neruda"
+    ]
+  },
+];
